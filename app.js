9736951848fd61db2b832b39f53698b5a6c600f7
skusuma-ub <skusuma@my.bridgeport.edu>
2022-12-07 19:14:38 -0500

    Full pipeline test4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CPSC-571-FALL-2022_NEW_commit_02'));
+app.get('/', (req, res) => res.send('CPSC-571-FALL-2022_NEW_commit_03'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
